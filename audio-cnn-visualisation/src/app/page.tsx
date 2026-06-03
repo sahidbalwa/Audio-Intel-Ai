239007970858d408382e8d5c5173f2a57f70bfec
sahidbalwa <138686668+sahidbalwa@users.noreply.github.com>
2026-06-03 12:24:27 +0530

    fixed .

--- audio-cnn-visualisation/src/app/page.tsx
+++ audio-cnn-visualisation/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/link";
+
 import { useState } from "react";
 import ColorScale from "~/components/ColorScale";
 import FeatureMap from "~/components/FeatureMap";
@@ -21,9 +21,7 @@ interface LayerData {
   values: number[][];
 }
 
-interface VisualizationData {
-  [layerName: string]: LayerData;
-}
+type VisualizationData = Record<string, LayerData>;
 
 interface WaveformData {
   values: number[];
@@ -92,7 +90,7 @@ const ESC50_EMOJI_MAP: Record<string, string> = {
 };
 
 const getEmojiForClass = (className: string): string => {
-  return ESC50_EMOJI_MAP[className] || "🔈";
+  return ESC50_EMOJI_MAP[className] ?? "🔈";
 };
 
 function splitLayers(visualization: VisualizationData) {
@@ -106,7 +104,7 @@ function splitLayers(visualization: VisualizationData) {
       const [parent] = name.split(".");
       if (parent === undefined) continue;
 
-      if (!internals[parent]) internals[parent] = [];
+      internals[parent] ??= [];
       internals[parent].push([name, data]);
     }
   }
@@ -153,7 +151,7 @@ export default function HomePage() {
           throw new Error(`API error ${response.statusText}`);
         }
 
-        const data: ApiResponse = await response.json();
+        const data = (await response.json()) as ApiResponse;
         setVizData(data);
       } catch (err) {
         setError(
@@ -189,7 +187,7 @@ export default function HomePage() {
             CNN Audio Visualizer
           </h1>
           <p className="text-md mb-8 text-stone-600">
-            Upload a WAV file to see the model's predictions and feauture maps
+            Upload a WAV file to see the model&apos;s predictions and feauture maps
           </p>
 
           <div className="flex flex-col items-center">
