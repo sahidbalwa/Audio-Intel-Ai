eaf70b1378312b9debd3c5692d94c0cb63711558
sahidbalwa <138686668+sahidbalwa@users.noreply.github.com>
2026-06-03 12:05:05 +0530

    Disable linting during Vercel build"

--- audio-cnn-visualisation/next.config.js
+++ audio-cnn-visualisation/next.config.js
@@ -6,9 +6,6 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
     typescript: {
         ignoreBuildErrors: true,
     },
